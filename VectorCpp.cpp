ea3f4843553c16460eb722bf0e087e4650fbf181
WhoAmI-FK <Faismen2004@mail.ru>
2022-03-13 23:12:39 +0100

    Vector update

--- VectorCpp.cpp
+++ VectorCpp.cpp
@@ -14,6 +14,65 @@ namespace whoami_vector {
             _right = r;
         }
     };
+    
+       
+   /* template <>
+    constexpr double default_value<double> = std::numeric_limits<double>::lowest();
+    */
+
+
+    template<class T>
+    class Iterator {
+    private:
+        node<T>* _ptr;
+    public:
+        explicit Iterator() : _ptr(nullptr) {
+
+        }
+        explicit Iterator(node<T>* ptr) : _ptr(ptr) {
+
+        }
+        bool operator==(const Iterator& r) const {
+            return this->_ptr == r._ptr;
+        }
+        bool operator!=(const Iterator& r) const {
+            return !(*this == r);
+        }
+        Iterator& operator++() {
+            if (_ptr->_right != nullptr) _ptr = _ptr->_right;
+            return *this;
+        }
+        Iterator& operator++(int) {
+            if (_ptr->_right != nullptr) {
+                Iterator<T> tmp(*this);
+                ++*this;
+                return tmp;
+            }
+        }
+        Iterator& operator+(int num) {
+            for (int i = 0; i < num; ++i) {
+                if (_ptr->_right != nullptr) {
+                    _ptr = _ptr->_right;
+                }
+                else break;
+            }
+            return *this;
+        }
+        Iterator& operator-(int num) {
+            for (int i = 0; i < num; ++i) {
+                if (_ptr->_left != nullptr) {
+                    _ptr = _ptr->_left;
+                }
+                else break;
+            }
+            return *this;
+        }
+         node<T>* operator*() {
+            return _ptr;
+        }
+    };
+    template <class T> T default_value = T{};
+
     template<class T>
     class Vector {
     private:
@@ -21,12 +80,12 @@ namespace whoami_vector {
         node<T>* _end;
         size_t _size;
     public:
-        Vector() {
+        explicit Vector() {
             _begin = nullptr;
             _end = nullptr;
             _size = 0;
         }
-        Vector(T data) {
+        explicit Vector(T data) {
             if (_begin == nullptr && _end == nullptr) {
                 _begin = new node<T>(data);
                 _end = _begin;
@@ -83,27 +142,55 @@ namespace whoami_vector {
         T& at(size_t index) const {
             return this->operator[](index);
         }
-
+        // need rework
+        void insert(Iterator<T> iterator, const T& val) {
+            node<T>* pointer = *iterator;
+            if (pointer == _begin && pointer != _end) {
+                node<T>* new_node = new node<T>(val, nullptr, _begin);
+                _begin = new_node;
+                _size++;
+                return;
+            }
+            if (pointer != _begin && pointer == _end) {
+                node<T>* new_node = new node<T>(val, _end, nullptr);
+                _end->_right = new_node;
+                _end = new_node;
+                _size++;
+                return;
+            }
+            if (pointer == _begin && pointer == _end) {
+                node<T>* new_node = new node<T>(val, nullptr, _begin);
+                _begin->_right = _end;
+                _begin = new_node;
+                _size++;
+                return;
+            }
+            if (pointer != _begin && pointer != _end) {
+                auto tmp = _begin;
+                while (tmp != pointer) {
+                    if(tmp->_right)
+                    tmp = tmp->_right;
+                }
+                 node<T>* new_node = new node<T>(val, tmp->_left, tmp);
+                    (tmp->_left)->_right = new_node;
+                    tmp->_left = new_node;
+                _size++;
+                return;
+            }
+        }
+       Iterator<T> begin() const {
+            return Iterator<T>(_begin);
+        }
+       Iterator<T> end() const {
+           return Iterator<T>(_end);
+       }
     };
 }
 
 int main()
 {
     try {
-        whoami_vector::Vector<int> vector(10);
-        vector.push_back(10);
-        vector.push_back(20);
-        vector.push_back(30);
-        for (size_t i = 0; i < vector.size(); ++i) {
-            std::cout << vector[i] << " ";
-        }
-        std::cout << std::endl;
-        vector.pop_back();
-        vector.pop_back();
-        vector.pop_back();
-        for (size_t i = 0; i < vector.size(); ++i) {
-            std::cout << vector[i] << " ";
-        }
+        //
     }
     catch (std::exception e) {
         std::cout << e.what() << std::endl;
